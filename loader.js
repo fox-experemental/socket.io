424d1923b7d354bcab717a999a1a031bae48df50
luchanso <oleg@loutchansky.com>
2016-04-24 21:00:37 +0300

    Add loader

---
+++ loader.js
@@ -0,0 +1,18 @@
+const cluster = require('cluster');
+const http = require('http');
+const numCPUs = require('os').cpus().length;
+
+if (cluster.isMaster) {
+  console.log(numCPUs);
+  for (var i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  console.log(`Worker ${cluster.worker.id} is run`);
+
+  require('./index');
+}
